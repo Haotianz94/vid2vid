5c7eb85cdb00c5c80c17e493c8d7c29972f4795a
haotianz <haotianz@stanford.edu>
2020-10-17 12:31:36 -0700

    fix dockerfile

--- docker/launch_docker.sh
+++ docker/launch_docker.sh
@@ -1,5 +1,12 @@
 # Thanks @dustinfreeman for providing the script
 #!/bin/bash
-sudo nvidia-docker build -t vid2vid:CUDA9-py35 .
+nvidia-docker build -f docker/Dockerfile -t vid2vid:CUDA9-py35 .
 
-sudo nvidia-docker run --rm -ti --ipc=host --shm-size 8G -v $(pwd):/vid2vid --workdir=/vid2vid vid2vid:CUDA9-py35 /bin/bash
+nvidia-docker run -ti --ipc=host --shm-size 32G -v $(pwd):/mnt_sora --workdir=/mnt_sora haotianz/vid2vid:CUDA9-py35 /bin/bash
+
+docker login
+
+docker tag vid2vid:CUDA9-py35 docker.io/haotianz/vid2vid:CUDA9-py35
+
+docker push haotianz/vid2vid:CUDA9-py35
+
